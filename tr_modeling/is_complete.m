41d72eb04dadd2ab78a2c6f0c5d575a4c66b75d7
Caio Giuliani <caio@giuliani.eng.br>
2020-05-29 14:15:39 -0300

    Simplified is_complete function

--- tr_modeling/is_complete.m
+++ tr_modeling/is_complete.m
@@ -1,17 +1,7 @@
 function result = is_complete(model)
-%IS_COMPLETE Summary of this function goes here
-%   Detailed explanation goes here
+%IS_COMPLETE True if there are no more places to include points
 
-    dim = size(model.center_point(), 1);
-    points_num = model.number_of_points();
-    
-    max_terms = ((dim + 1)*(dim + 2))/2;
-    max_terms_unused = length(model.pivot_polynomials);
-    
-    result = points_num >= max_terms;
-    if points_num > max_terms
-        warning('cmg:possible_error', 'Too many points');
-    end
+    result = isempty(find(model.pivot_values == 0, 1));
 
 end
 
