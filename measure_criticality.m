978ea405ba071c77707a932e99e64da05252b2e4
Caio Giuliani <caio@giuliani.eng.br>
2020-05-29 14:23:18 -0300

    Changed criticality measure

--- measure_criticality.m
+++ measure_criticality.m
@@ -2,28 +2,14 @@ function m = measure_criticality(model, constraints)
     % MEASURE_CRITICALITY gives the gradient of the model, calculated
     % in absolute coordinates in the current point.
     %
-    %    - In the present work, the polynomial model is being calculated
-    %    scaled, inside a ball of radius 1. Thus it has to be rescaled to
-    %    absolute coordinates.
-    %    
-    %    - If one desires handle constraints, other criticality
-    %    measures need to be considered
 
-    if isfield(constraints, 'lb') && ~isempty(constraints.lb)
-        lb = constraints.lb;
-    else
-        lb = -inf;
-    end
-    if isfield(constraints, 'ub') && ~isempty(constraints.ub)
-        ub = constraints.ub;
-    else
-        ub = inf;
-    end
     % Just the gradient, measured on the tr_center
     [~, grad] = get_model_matrices(model, 0);
     
     % Projected gradient
     x_center = model.center_point();
-    m = min(ub, max(lb, (x_center - grad))) - x_center;
+
+    m = project_to_feasible_polytope(x_center - grad, constraints) ...
+        - x_center;
 
 end
